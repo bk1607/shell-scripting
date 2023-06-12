1bc5ab90ffbebbf5ce1c95e3dd95e84730b5b689
bk1607 <baluanumala16@gmail.com>
2023-06-12 17:14:09 +0530

    if condition

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
-echo -e "\e[33m installing nginx \e[0m"
+echo -e "\e[33mInstalling nginx \e[0m"
 yum install nginx -y &> /var/log/roboshop.log
-if [ $? -gt 0 ];then
+if [ $? -lt 1 ];then
   echo -e "\e[32mSuccess\e[0m"
 else
   echo -e "\e[31mError occurred check /var/log/roboshop.log file\e[0m"
