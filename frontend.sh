85cc58f8164b2d2030dc6816cf355926adafbe2d
bk1607 <baluanumala16@gmail.com>
2023-06-12 17:09:39 +0530

    if condition

--- frontend.sh
+++ frontend.sh
@@ -1,2 +1,7 @@
-echo -e "\e[31m installing nginx \e[0m"
-yum install nginx -y
+echo -e "\e[33m installing nginx \e[0m"
+yum install nginx -y &> /var/log/roboshop.log
+if [ $? -gt 0 ];then
+  echo -e "\e[32mSuccess\e[0m"
+else
+  echo -e "\e[31mError occurred check /var/log/roboshop.log file\e[0m"
+fi
