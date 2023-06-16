83188710ba3e033bd276091448c65a10b3d7ba4a
bk1607 <baluanumala16@gmail.com>
2023-06-16 12:06:41 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,3 @@
 source common.sh
-print_head "setup nodejs repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> "${log_file}"
-error_check
-
-print_head "Installing nodejs"
-yum install nodejs -y &>> "${log_file}"
-error_check
-
+component=catalogue
+nodejs
